af5009c54347e565ac794bcce0619e3920ba7977
thelastinuit <email+919@aloui.se>
2023-05-31 11:38:22 -0600

    Add subscriber.

--- subscriber/src/main.rs
+++ subscriber/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use borsh::{BorshSerialize, BorshDeserialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler, QueueProperties};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct ItemCreatedEventMessage {
+    pub item_id: String,
+    pub item_title: String,
+}
+
+pub struct ItemCreatedHandler;
+
+impl MessageHandler<ItemCreatedEventMessage> for ItemCreatedHandler {
+    fn handle(&self, message: Box<ItemCreatedEventMessage>) -> Result<(), HandleError> {
+        println!("Message received on handler: {:?}", message);
+        Ok(())
+    }
+
+    fn get_handler_action(&self) -> String { "item_created".to_owned() }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@0.0.0.0:5672".to_owned())?;
+    _ = listener.listen(
+        "item_created".to_owned(),
+        ItemCreatedHandler,
+        QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
 }
